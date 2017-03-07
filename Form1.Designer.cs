03f9a25b26f0fe1bf48f3d3ed97b39ce16e66c35
Matheus Pereira <pereiramateduardo@gmail.com>
2017-03-07 18:27:19 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace MM_Compiler
+{
+    partial class FormCompiler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCompiler));
+            this.ToolBox = new System.Windows.Forms.Panel();
+            this.ButtonEquipe = new System.Windows.Forms.Button();
+            this.ButtonGerarCodigo = new System.Windows.Forms.Button();
+            this.ButtonCompilar = new System.Windows.Forms.Button();
+            this.ButtonRecortar = new System.Windows.Forms.Button();
+            this.ButtonColar = new System.Windows.Forms.Button();
+            this.ButtonCopiar = new System.Windows.Forms.Button();
+            this.ButtonSalvar = new System.Windows.Forms.Button();
+            this.ButtonAbrir = new System.Windows.Forms.Button();
+            this.ButtonNovo = new System.Windows.Forms.Button();
+            this.TextEditor = new System.Windows.Forms.RichTextBox();
+            this.MessageDisplay = new System.Windows.Forms.RichTextBox();
+            this.StatusBar = new System.Windows.Forms.Panel();
+            this.LabelPath = new System.Windows.Forms.Label();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.ToolBox.SuspendLayout();
+            this.StatusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ToolBox
+            // 
+            this.ToolBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ToolBox.Controls.Add(this.ButtonEquipe);
+            this.ToolBox.Controls.Add(this.ButtonGerarCodigo);
+            this.ToolBox.Controls.Add(this.ButtonCompilar);
+            this.ToolBox.Controls.Add(this.ButtonRecortar);
+            this.ToolBox.Controls.Add(this.ButtonColar);
+            this.ToolBox.Controls.Add(this.ButtonCopiar);
+            this.ToolBox.Controls.Add(this.ButtonSalvar);
+            this.ToolBox.Controls.Add(this.ButtonAbrir);
+            this.ToolBox.Controls.Add(this.ButtonNovo);
+            this.ToolBox.Location = new System.Drawing.Point(3, 2);
+            this.ToolBox.Name = "ToolBox";
+            this.ToolBox.Size = new System.Drawing.Size(214, 576);
+            this.ToolBox.TabIndex = 0;
+            // 
+            // ButtonEquipe
+            // 
+            this.ButtonEquipe.Location = new System.Drawing.Point(0, 513);
+            this.ButtonEquipe.Name = "ButtonEquipe";
+            this.ButtonEquipe.Size = new System.Drawing.Size(144, 63);
+            this.ButtonEquipe.TabIndex = 4;
+            this.ButtonEquipe.Text = "Equipe [F1]";
+            this.ButtonEquipe.UseVisualStyleBackColor = true;
+            this.ButtonEquipe.Click += ButtonEquipe_Click;
+            // 
+            // ButtonGerarCodigo
+            // 
+            this.ButtonGerarCodigo.Location = new System.Drawing.Point(0, 448);
+            this.ButtonGerarCodigo.Name = "ButtonGerarCodigo";
+            this.ButtonGerarCodigo.Size = new System.Drawing.Size(144, 63);
+            this.ButtonGerarCodigo.TabIndex = 4;
+            this.ButtonGerarCodigo.Text = "Gerar Código [F9]";
+            this.ButtonGerarCodigo.UseVisualStyleBackColor = true;
+            this.ButtonGerarCodigo.Click += ButtonGerarCodigo_Click;
+            // 
+            // ButtonCompilar
+            // 
+            this.ButtonCompilar.Location = new System.Drawing.Point(0, 384);
+            this.ButtonCompilar.Name = "ButtonCompilar";
+            this.ButtonCompilar.Size = new System.Drawing.Size(144, 63);
+            this.ButtonCompilar.TabIndex = 4;
+            this.ButtonCompilar.Text = "Compilar [F8]";
+            this.ButtonCompilar.UseVisualStyleBackColor = true;
+            this.ButtonCompilar.Click += ButtonCompilar_Click;
+            // 
+            // ButtonRecortar
+            // 
+            this.ButtonRecortar.Location = new System.Drawing.Point(0, 320);
+            this.ButtonRecortar.Name = "ButtonRecortar";
+            this.ButtonRecortar.Size = new System.Drawing.Size(144, 63);
+            this.ButtonRecortar.TabIndex = 4;
+            this.ButtonRecortar.Text = "Recortar [Ctrl+X]";
+            this.ButtonRecortar.UseVisualStyleBackColor = true;
+            this.ButtonRecortar.Click += ButtonRecortar_Click;
+            // 
+            // ButtonColar
+            // 
+            this.ButtonColar.Location = new System.Drawing.Point(0, 256);
+            this.ButtonColar.Name = "ButtonColar";
+            this.ButtonColar.Size = new System.Drawing.Size(144, 63);
+            this.ButtonColar.TabIndex = 4;
+            this.ButtonColar.Text = "Colar [Ctrl+V]";
+            this.ButtonColar.UseVisualStyleBackColor = true;
+            this.ButtonColar.Click += ButtonColar_Click;
+            // 
+            // ButtonCopiar
+            // 
+            this.ButtonCopiar.Location = new System.Drawing.Point(0, 192);
+            this.ButtonCopiar.Name = "ButtonCopiar";
+            this.ButtonCopiar.Size = new System.Drawing.Size(144, 63);
+            this.ButtonCopiar.TabIndex = 4;
+            this.ButtonCopiar.Text = "Copiar [Ctrl+C]";
+            this.ButtonCopiar.UseVisualStyleBackColor = true;
+            this.ButtonCopiar.Click += ButtonCopiar_Click;
+            // 
+            // ButtonSalvar
+            // 
+            this.ButtonSalvar.Location = new System.Drawing.Point(0, 128);
+            this.ButtonSalvar.Name = "ButtonSalvar";
+            this.ButtonSalvar.Size = new System.Drawing.Size(144, 63);
+            this.ButtonSalvar.TabIndex = 4;
+            this.ButtonSalvar.Text = "Salvar [Ctrl+S]";
+            this.ButtonSalvar.UseVisualStyleBackColor = true;
+            this.ButtonSalvar.Click += ButtonSalvar_Click;
+            // 
+            // ButtonAbrir
+            // 
+            this.ButtonAbrir.Location = new System.Drawing.Point(0, 64);
+            this.ButtonAbrir.Name = "ButtonAbrir";
+            this.ButtonAbrir.Size = new System.Drawing.Size(144, 63);
+            this.ButtonAbrir.TabIndex = 4;
+            this.ButtonAbrir.Text = "Abrir [Ctrl+O]";
+            this.ButtonAbrir.UseVisualStyleBackColor = true;
+            this.ButtonAbrir.Click += ButtonAbrir_Click;
+            // 
+            // ButtonNovo
+            // 
+            this.ButtonNovo.Location = new System.Drawing.Point(0, 0);
+            this.ButtonNovo.Name = "ButtonNovo";
+            this.ButtonNovo.Size = new System.Drawing.Size(144, 63);
+            this.ButtonNovo.TabIndex = 2;
+            this.ButtonNovo.Text = "Novo [Ctrl+N]";
+            this.ButtonNovo.UseVisualStyleBackColor = true;
+            this.ButtonNovo.Click += ButtonNovo_Click;
+            // 
+            // TextEditor
+            // 
+            this.TextEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextEditor.Location = new System.Drawing.Point(153, 2);
+            this.TextEditor.Name = "TextEditor";
+            this.TextEditor.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.TextEditor.Size = new System.Drawing.Size(820, 480);
+            this.TextEditor.TabIndex = 1;
+            this.TextEditor.Text = "";
+            this.TextEditor.WordWrap = false;
+            this.TextEditor.TextChanged += TextEditor_TextChanged;
+            // 
+            // MessageDisplay
+            // 
+            this.MessageDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MessageDisplay.Location = new System.Drawing.Point(153, 488);
+            this.MessageDisplay.Name = "MessageDisplay";
+            this.MessageDisplay.ReadOnly = true;
+            this.MessageDisplay.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.MessageDisplay.Size = new System.Drawing.Size(820, 90);
+            this.MessageDisplay.TabIndex = 2;
+            this.MessageDisplay.Text = "";
+            this.MessageDisplay.WordWrap = false;
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusBar.Controls.Add(this.LabelPath);
+            this.StatusBar.Controls.Add(this.LabelStatus);
+            this.StatusBar.Location = new System.Drawing.Point(3, 584);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Size = new System.Drawing.Size(970, 42);
+            this.StatusBar.TabIndex = 3;
+            // 
+            // LabelArquivo
+            // 
+            this.LabelPath.AutoSize = true;
+            this.LabelPath.Location = new System.Drawing.Point(3, 23);
+            this.LabelPath.Name = "LabelPath";
+            this.LabelPath.Size = new System.Drawing.Size(0, 13);
+            this.LabelPath.TabIndex = 1;
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.AutoSize = true;
+            this.LabelStatus.Location = new System.Drawing.Point(3, 6);
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(81, 13);
+            this.LabelStatus.TabIndex = 0;
+            this.LabelStatus.Text = "Não modificado";
+            // 
+            // FormCompiler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 629);
+            this.Controls.Add(this.StatusBar);
+            this.Controls.Add(this.MessageDisplay);
+            this.Controls.Add(this.TextEditor);
+            this.Controls.Add(this.ToolBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(992, 668);
+            this.Name = "FormCompiler";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MM Compiler";
+            this.ToolBox.ResumeLayout(false);
+            this.StatusBar.ResumeLayout(false);
+            this.StatusBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Panel ToolBox;
+        private System.Windows.Forms.RichTextBox MessageDisplay;
+        private System.Windows.Forms.Panel StatusBar;
+        private System.Windows.Forms.Label LabelStatus;
+        private System.Windows.Forms.Label LabelPath;
+        private System.Windows.Forms.Button ButtonNovo;
+        private System.Windows.Forms.Button ButtonAbrir;
+        private System.Windows.Forms.Button ButtonCopiar;
+        private System.Windows.Forms.Button ButtonSalvar;
+        private System.Windows.Forms.Button ButtonEquipe;
+        private System.Windows.Forms.Button ButtonGerarCodigo;
+        private System.Windows.Forms.Button ButtonCompilar;
+        private System.Windows.Forms.Button ButtonRecortar;
+        private System.Windows.Forms.Button ButtonColar;
+        private System.Windows.Forms.RichTextBox TextEditor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Text;
+
+namespace MM_Compiler
+{
+    public partial class FormCompiler : Form
+    {
+        public FormCompiler()
+        {
+            InitializeComponent();
+        }
+
+        #region Métodos sobre a View
+        private void ButtonEquipe_Click(object sender, EventArgs e)
+        {
+            this.MessageDisplay.Text = Environment.NewLine + "Equipe: Matheus Adriano Pereira e Matheus Eduardo Hoeltgebaum Pereira.";
+        }
+
+        private void ButtonGerarCodigo_Click(object sender, EventArgs e)
+        {
+            this.MessageDisplay.Text = Environment.NewLine + "Geração de código ainda não foi implementada.";
+        }
+
+        private void ButtonCompilar_Click(object sender, EventArgs e)
+        {
+            this.MessageDisplay.Text = Environment.NewLine + "Compilação de programas ainda não foi implementada.";
+        }
+
+        private void ButtonRecortar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextEditor.SelectedText))
+                TextEditor.Cut();
+        }
+
+        private void ButtonColar_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                TextEditor.Paste();
+            }
+        }
+
+        private void ButtonCopiar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextEditor.SelectedText))
+                TextEditor.Copy();
+        }
+
+        private void ButtonSalvar_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveDialog = new SaveFileDialog())
+            {
+                saveDialog.Filter = "Arquivos de texto|*.txt";
+                saveDialog.Title = "Selecione um local para salvar o arquivo";
+
+                if (!File.Exists(LabelPath.Text))
+                {
+                    if (saveDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        CriarArquivo(saveDialog.FileName);
+                        this.MessageDisplay.Clear();
+                        this.LabelPath.Text = saveDialog.FileName;
+                        this.LabelStatus.Text = "Não modificado";
+                    }
+                }
+                else
+                {
+                    SalvarArquivo();
+                    this.MessageDisplay.Clear();
+                    this.LabelStatus.Text = "Não modificado";
+                }
+            }
+        }
+
+        private void CriarArquivo(string path)
+        {
+            try
+            {
+                using (FileStream fileObject = File.Create(path))
+                {
+                    byte[] conteudo = Encoding.GetEncoding("ISO-8859-1").GetBytes(TextEditor.Text);
+                    fileObject.Write(conteudo, 0, conteudo.Length);
+                }
+            }
+            catch
+            {
+                MessageDisplay.Text += "\n Não foi possível salvar o arquivo";
+            }
+        }
+
+        private void SalvarArquivo()
+        {
+            try
+            {
+                using (FileStream fileObject = File.Open(LabelPath.Text, FileMode.Open))
+                {
+                    byte[] conteudo = Encoding.GetEncoding("ISO-8859-1").GetBytes(TextEditor.Text);
+                    fileObject.Write(conteudo, 0, conteudo.Length);
+                }
+            }
+            catch
+            {
+                MessageDisplay.Text += "\n Não foi possível salvar as alterações realizadas no arquivo.";
+            }
+        }
+
+        private void ButtonAbrir_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openDialog = new OpenFileDialog())
+            {
+                openDialog.Filter = "Arquivos de texto|*.txt";
+                openDialog.Title = "Selecione um arquivo";
+
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    CarregarConteudoArquivo(openDialog.OpenFile());
+                    this.MessageDisplay.Clear();
+                    this.LabelPath.Text = openDialog.FileName;
+                    this.LabelStatus.Text = "Não modificado";
+                }
+            }
+        }
+
+        private void CarregarConteudoArquivo(Stream fileObject)
+        {
+            try
+            {
+                byte[] tamanhoBytesFile = new byte[fileObject.Length];
+                fileObject.Read(tamanhoBytesFile, 0, Convert.ToInt32(fileObject.Length));
+                fileObject.Flush();
+                fileObject.Close();
+                this.TextEditor.Text = Encoding.GetEncoding("ISO-8859-1").GetString(tamanhoBytesFile);
+            }
+            catch
+            {
+                this.MessageDisplay.Text += "\n Não foi possível carregar o arquivo selecionado.";
+            }
+        }
+
+        private void ButtonNovo_Click(object sender, EventArgs e)
+        {
+            this.TextEditor.Clear();
+            this.MessageDisplay.Clear();
+            this.LabelPath.Text = "";
+            this.LabelStatus.Text = "Não modificado";
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            KeyEventArgs e = new KeyEventArgs(keyData);
+
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                this.ButtonCopiar.PerformClick();
+                return true;
+            }
+            else if (e.Control && e.KeyCode == Keys.N)
+            {
+                this.ButtonNovo.PerformClick();
+                return true;
+            }
+            else if (e.Control && e.KeyCode == Keys.O)
+            {
+                this.ButtonAbrir.PerformClick();
+                return true;
+            }
+            else if (e.Control && e.KeyCode == Keys.S)
+            {
+                this.ButtonSalvar.PerformClick();
+                return true;
+            }
+            else if (e.Control && e.KeyCode == Keys.V)
+            {
+                this.ButtonColar.PerformClick();
+                return true;
+            }
+            else if (e.Control && e.KeyCode == Keys.X)
+            {
+                this.ButtonRecortar.PerformClick();
+                return true;
+            }
+            else if (e.KeyCode == Keys.F1)
+            {
+                this.ButtonEquipe.PerformClick();
+            }
+            else if (e.KeyCode == Keys.F8)
+            {
+                this.ButtonCompilar.PerformClick();
+            }
+            else if (e.KeyCode == Keys.F9)
+            {
+                this.ButtonGerarCodigo.PerformClick();
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void TextEditor_TextChanged(object sender, EventArgs e)
+        {
+            if (!LabelStatus.Text.Equals("Modificado"))
+                this.LabelStatus.Text = "Modificado";
+        }
+        #endregion
+    }
+}
